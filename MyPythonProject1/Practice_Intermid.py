8a61dcbc845b5c652b0897f5bebf0be53585d4c4
ItsMistahJi <balajir22@icloud.com>
2025-03-27 07:53:14 +0000

    create and learn class and inheritance

--- MyPythonProject1/Practice_Intermid.py
+++ MyPythonProject1/Practice_Intermid.py
@@ -57,22 +57,93 @@ dog1=Dog("Tom1")
 dog2=Dog("Tom2")
 
 dog1.bark()
-dog2.bark()'''
+dog2.bark()
 
+#my own parent class, child classes
+class shape:
+    print("this is parent class Shape")
+
+    def __init__(self,height=0,width=0):
+        print("inside init of class shape")
 class square:
-    sides = 4
+    info = "This is child class of Shape - Square"
+    print(info)
+    #sides = 4
 
-    def __init__(self):
+    def __init__(self,height=0,width=0):
         self.height = 0
         self.width = 0 
+        print("Inside init class of square")
     
     def caculate_area(self):
         return self.height * self.width
 
+class rectangle(shape):
+    print("this is child class of shape")
 
-square1 = square()
-square1.height = int(input("Enter height: "))
-square1.width = int(input("Enter width: "))
-print(square1.caculate_area())
+    def __init__(self,height=0,width=0):
+        super().__init__(self)
+        print("Inside init class of rectangle")
+    
+    def caculate_area(self):
+        return self.height * self.width * 2
 
+def main():
+    #shape()
+    sq1 = square()
+    rec1 = rectangle()
+    sq1.height = int(input("Enter height: "))
+    sq1.width = int(input("Enter width: "))
+    rec1.width = int(input("Enter width: "))
+    rec1.height = int(input("Enter height: "))
+    print(sq1.caculate_area())
+    print(rec1.caculate_area())
 
+main()'''
+
+class numbers():
+    #print("inside class numbers")
+
+    def __init__(self,num):
+        #print("inside init of numbers")
+        self.num = num
+
+class even(numbers):
+    #print("inside class even")
+
+    def __init__(self,num):
+        super().__init__(num)
+        #print("inside init of even")
+    
+    def check_even(self):
+        if self.num % 2 == 0:
+            print(f"{self.num} is an even number")
+        else:
+            print(f"{self.num} is not an even number")
+class prime(numbers):
+    #print("inside class prime")
+
+    def __init__(self,num):
+        #print("inside init of prime")
+        super().__init__(num)
+
+    def check_prime(self):
+        if self.num > 1:
+            for i in range(2,self.num):
+                if self.num % i == 0:
+                    print(f"{self.num} is not a prime number")
+                    break
+            else:
+                print(f"{self.num} is a prime number")
+        else:
+            print(f"{self.num} is not a prime number")
+        
+
+def main():
+    num = int(input("Enter a number: "))
+    even1 = even(num)
+    prime1 = prime(num)
+    even1.check_even()
+    prime1.check_prime()
+
+main()
