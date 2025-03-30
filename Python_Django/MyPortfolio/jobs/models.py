594d8cdff9fdb417d60c2599548b3dc2eec13fda
ItsMistahJi <152993670+ItsMistahJi@users.noreply.github.com>
2025-03-30 21:26:23 +0100

    Python django site host
    pending error to sort out image loading.

--- Python_Django/MyPortfolio/jobs/models.py
+++ Python_Django/MyPortfolio/jobs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    #Title of the project
+    title = models.CharField(max_length=200)
+    #description of the project
+    description = models.TextField()
+    #link to view projects if any
+    link = models.URLField(blank=True,null=True)
+    #image for the project
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.title
+
--- Python_Django/MyPortfolio/jobs/views.py
+++ Python_Django/MyPortfolio/jobs/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from .models import Job
 
 def jobs(request):
-    template = loader.get_template('SamplePortfolio.html')
-    return HttpResponse(template.render())
+    #fetch all jobs from the database
+    jobs=Job.objects.all()
+    return render(request,'SamplePortfolio.html',{'jobs':jobs})
+#    template = loader.get_template('SamplePortfolio.html')
+#    return HttpResponse(template.render())
